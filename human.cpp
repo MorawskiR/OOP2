814ed788470308a652b8802d06cc9ac8d237bcf9
Rafal <r.morawski92@gmail.com>
2023-11-19 09:00:51 +0100

    c-tors

--- human.cpp
+++ human.cpp
@@ -5,3 +5,15 @@
 #include "human.hpp"
 
 
+Human::Human(){
+    name_ = "";
+    age_ = 0;
+}
+// Human::Human(std::string name, size_t age){
+//     name_ = name;
+//     age_ = age;
+// }
+void Human::Display()
+{
+    std::cout<<"name: "<<name_<<", "<<"age: "<<age_<<"\n";
+}
--- human.hpp
+++ human.hpp
@@ -4,8 +4,16 @@
 class Human{
 public:
     Human();
+    //Human(std::string name, size_t age);
+
+    Human(std::string name, size_t age)
+    : name_(name), age_(age) {}
+    
+    Human(std::string name)
+    :Human(name, 50){}
+
+    void Display();
 private:
     std::string name_;
     size_t age_;
-
 };
