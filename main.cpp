ac56ac32a7a9f70b79de6daf5f7b49742d40b8ae
Rafal <r.morawski92@gmail.com>
2023-11-27 21:08:37 +0100

    clear2

--- main.cpp
+++ main.cpp
@@ -1,18 +1,4 @@
 
-#include "human.hpp"
 int main(){
 
-    // Human adam{"Adam", 25};
-    // adam.getName();
-    // adam.getAge();
-
-    // Human Ewa{"Ewa"}; 
-    // Ewa.Display();
-
-    Human Pan;
-    Pan.setName("Henry");
-    Pan.setAge(33);
-   
-}  // Human adam{"Adam", 25};
-    // adam.getName();
-    // adam.getAge();mdcoid d g
+}
